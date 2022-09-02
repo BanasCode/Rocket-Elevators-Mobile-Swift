d8e8108d55b00985aceea793742723da4ea66d9d
BanasCode <ryanzbanas@gmail.com>
2022-09-02 11:49:01 -0400

    Initial Commit

---
+++ Re-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Re-app
+//
+//  Created by Ryan Banas on 9/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Re-app/Re_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Re_appApp.swift
+//  Re-app
+//
+//  Created by Ryan Banas on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Re_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
